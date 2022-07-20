b483273262b63a580da9921deef3ebdd1bfeff58
Abdellatif Laghjaj <abdelatiflaghjaj@gmail.com>
2022-07-20 18:11:17 +0100

    random user

--- js/app.js
+++ js/app.js
@@ -7,7 +7,19 @@ const app = Vue.createApp({
             gender: "male",
             picture: "https://randomuser.me/api/portraits/men/1.jpg"
         }
-    }
+    },
+    methods: {
+        async getRandomUser(){
+            const res = await fetch("https://randomuser.me/api/");
+            const {data} = await res.json();
+
+            this.first_name = data[0].name.first;
+            this.last_name = data[0].name.last;
+            this.email = data[0].email;
+            this.gender = data[0].gender;
+            this.picture = data[0].picture.large;
+        }
+    },
 }) 
 
 app.mount("#app");
